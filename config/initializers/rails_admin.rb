053cd6303de84653109673204b7d91b2c99443cc
qdrait <qdrait@gmail.com>
2018-09-27 13:34:43 +0900

    rails_adminを日本語化

--- config/initializers/rails_admin.rb
+++ config/initializers/rails_admin.rb
@@ -38,4 +38,125 @@ RailsAdmin.config do |config|
     # history_index
     # history_show
   end
+
+  config.excluded_models = ["Form::Answer","Form::AnswerDetail",]
+
+  config.model 'Answer' do
+    LABEL_id = '回答ID'
+    LABEL_user = 'ユーザ'
+    LABEL_office = '店舗'
+    LABEL_answer_details = '回答詳細'
+    LABEL_created_at = '作成日時'
+    LABEL_updated_at = '更新日時'
+
+    list do
+      field :id do
+        label LABEL_id
+      end
+      field :user do
+        label LABEL_user
+      end
+      field :office do
+        label LABEL_office
+      end
+      field :answer_details do
+        label LABEL_answer_details
+      end
+      field :created_at do
+        label LABEL_created_at
+      end
+      field :updated_at do
+        label LABEL_updated_at
+      end
+    end
+  end
+
+
+  config.model 'AnswerDetail' do
+    LABEL_id = '回答詳細ID'
+    LABEL_answer = '回答'
+    LABEL_question_no = '設問NO'
+    LABEL_content = '回答内容'
+    LABEL_created_at = '作成日時'
+    LABEL_updated_at = '更新日時'
+
+    list do
+      field :id do
+        label LABEL_id
+      end
+      field :answer do
+        label LABEL_answer
+      end
+      field :question_no do
+        label LABEL_question_no
+      end
+      field :content do
+        label LABEL_content
+      end
+      field :created_at do
+        label LABEL_created_at
+      end
+      field :updated_at do
+        label LABEL_updated_at
+      end
+    end
+  end
+
+
+  def custom_label_method
+    self.email
+  end
+
+  config.model 'User' do
+    LABEL_id = 'ユーザID'
+    LABEL_email = 'メールアドレス'
+    LABEL_created_at = '作成日時'
+    LABEL_updated_at = '更新日時'
+
+    object_label_method do
+      :custom_label_method
+    end
+
+    list do
+      field :id do
+        label LABEL_id
+      end
+      field :email do
+        label LABEL_email
+      end
+      field :created_at do
+        label LABEL_created_at
+      end
+      field :updated_at do
+        label LABEL_updated_at
+      end
+    end
+  end
+
+  config.model 'Office' do
+    LABEL_id = '店舗ID'
+    LABEL_name = '店舗名'
+    LABEL_message = '店舗メッセージ'
+    LABEL_created_at = '作成日時'
+    LABEL_updated_at = '更新日時'
+
+    list do
+      field :id do
+        label LABEL_id
+      end
+      field :name do
+        label LABEL_name
+      end
+      field :message do
+        label LABEL_message
+      end
+      field :created_at do
+        label LABEL_created_at
+      end
+      field :updated_at do
+        label LABEL_updated_at
+      end
+    end
+  end
+
 end
